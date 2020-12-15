354b1ce42b97052346bb081810f0204a347c8130
Shreyas Gupta <technology.shreyas@gmail.com>
2020-12-15 10:50:11 +0530

    add day 15, cpp today!

---
+++ 15/code-2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    map<int, int> m;
+    int c, turn=1, last=0, lastog = 0;
+    bool first = true;
+    while(cin>>c) {
+        lastog = m[c];
+        m[c] = turn;
+        last = c;
+        turn++;
+    }
+    while(1) {
+        int ans = 0;
+        if(first) {
+            ans = 0;
+        } else {
+            ans = (turn-1) - lastog;
+        }
+
+        if(m[ans]==0) {
+            first = true;
+        } else {
+            first = false;
+        }
+        last = ans;
+        lastog = m[ans];
+        m[ans] = turn;
+        turn++;
+        cout<<turn<<" - "<<ans<<endl;
+        if(turn==30000001) break;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    long long t=1;
+    //cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
